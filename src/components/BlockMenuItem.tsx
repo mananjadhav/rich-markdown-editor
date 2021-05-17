ef722814c8754f09d78085fda943b72fbbdcca3c
Manan Jadhav <manan.jadhav@gmail.com>
2021-05-18 00:15:58 +0530

    feat: Fixed BlockMenuItem layout

--- src/components/BlockMenuItem.tsx
+++ src/components/BlockMenuItem.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import scrollIntoView from "smooth-scroll-into-view-if-needed";
 import styled, { withTheme } from "styled-components";
 import theme from "../theme";
+import DefaultTooltip from "./Tooltip";
 
 type Props = {
   selected: boolean;
@@ -11,6 +12,7 @@ type Props = {
   icon: typeof React.Component | React.FC<any>;
   title: string;
   shortcut?: string;
+  tooltip?: typeof React.Component | React.FC<any>;
 };
 
 function BlockMenuItem({
@@ -20,16 +22,18 @@ function BlockMenuItem({
   title,
   shortcut,
   icon,
+  tooltip = DefaultTooltip,
 }: Props) {
   const Icon = icon;
+  const Tooltip = tooltip;
 
   const ref = React.useCallback(
-    node => {
+    (node) => {
       if (selected && node) {
         scrollIntoView(node, {
           scrollMode: "if-needed",
           block: "center",
-          boundary: parent => {
+          boundary: (parent) => {
             // All the parent elements of your target are checked until they
             // reach the #block-menu-container. Prevents body and other parent
             // elements from being scrolled
@@ -42,19 +46,19 @@ function BlockMenuItem({
   );
 
   return (
-    <MenuItem
-      selected={selected}
-      onClick={disabled ? undefined : onClick}
-      ref={ref}
-    >
-      <Icon
-        color={
-          selected ? theme.blockToolbarIconSelected : theme.blockToolbarIcon
-        }
-      />
-      &nbsp;&nbsp;{title}
-      <Shortcut>{shortcut}</Shortcut>
-    </MenuItem>
+    <Tooltip tooltip={`${title}    |    ${shortcut}`} placement="bottom">
+      <MenuItem
+        selected={selected}
+        onClick={disabled ? undefined : onClick}
+        ref={ref}
+      >
+        <Icon
+          color={
+            selected ? theme.blockToolbarIconSelected : theme.blockToolbarIcon
+          }
+        />
+      </MenuItem>
+    </Tooltip>
   );
 }
 
@@ -71,20 +75,20 @@ const MenuItem = styled.button<{
   height: 36px;
   cursor: pointer;
   border: none;
-  opacity: ${props => (props.disabled ? ".5" : "1")};
-  color: ${props =>
+  opacity: ${(props) => (props.disabled ? ".5" : "1")};
+  color: ${(props) =>
     props.selected
       ? props.theme.blockToolbarTextSelected
       : props.theme.blockToolbarText};
-  background: ${props =>
+  background: ${(props) =>
     props.selected ? props.theme.blockToolbarTrigger : "none"};
   padding: 0 16px;
   outline: none;
 
   &:hover,
   &:active {
-    color: ${props => props.theme.blockToolbarTextSelected};
-    background: ${props =>
+    color: ${(props) => props.theme.blockToolbarTextSelected};
+    background: ${(props) =>
       props.selected
         ? props.theme.blockToolbarTrigger
         : props.theme.blockToolbarHoverBackground};
@@ -92,7 +96,7 @@ const MenuItem = styled.button<{
 `;
 
 const Shortcut = styled.span`
-  color: ${props => props.theme.textSecondary};
+  color: ${(props) => props.theme.textSecondary};
   flex-grow: 1;
   text-align: right;
 `;
